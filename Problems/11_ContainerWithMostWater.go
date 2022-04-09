b3a9cb0f785f3467dc1f126b62e190f48decf26c
RueiYang <s35016080@gmail.com>
2022-04-09 14:04:01 +0800

    完成第11題

--- Problems/11_ContainerWithMostWater.go
+++ Problems/11_ContainerWithMostWater.go
@@ -17,27 +17,14 @@ func MaxArea(height []int) int {
 			fmt.Println("更大面積出現", area)
 			result = area
 		} else {
-			fmt.Println("沒比較大")
-			// 如果左邊比較短
-			if side == "L" {
-				fmt.Println("左邊比較短")
-				// 左邊的下一個又比現在的左邊短，則跳過他
-				if height[i+1] < shorter {
-					fmt.Println("下一個左邊又更短，跳過")
-					i++
-				}
-			} else {
-				fmt.Println("右邊比較短")
-				// 如果右邊比較短
-				// 右邊的下一個比現在的右邊長，則j--
-				if height[j-1] > shorter {
-					fmt.Println("下一個右邊比較長，j--")
-					i-- // 此時i應該不要動，所以先--，因為等等會被++
-					j--
-				} else {
-					j--
-				}
-			}
+			fmt.Println("面積沒比較大")
+		}
+		if side == "L" {
+			fmt.Println("左邊比較短，左邊往中間一步") // i++
+		} else {
+			fmt.Println("右邊比較短，右邊往中間一步") // j--，i--(因為i等等會被++)
+			j--
+			i--
 		}
 	}
 	return result
