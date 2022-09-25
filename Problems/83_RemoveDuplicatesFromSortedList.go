38f41b5dbf8711c17cfab2c777b82dbccf657018
RueiYang <s35016080@gmail.com>
2022-09-25 22:20:19 +0800

    83刪掉第一次寫的大便

--- Problems/83_RemoveDuplicatesFromSortedList.go
+++ Problems/83_RemoveDuplicatesFromSortedList.go
@@ -1,30 +1,42 @@
 package problems
 
 func RemoveDuplicatesFromSortedList(head *ListNode) *ListNode {
-	if head == nil {
-		return nil
-	}
-	var resultHead, resultCurrent *ListNode
-	existMap := map[int]interface{}{}
-
-	currentNode := head
-	for {
-		if currentNode == nil {
-			break
-		} else {
-			if _, exist := existMap[currentNode.Val]; !exist {
-				if resultHead == nil {
-					resultHead = &ListNode{Val: currentNode.Val}
-					resultCurrent = resultHead
-				} else {
-					resultCurrent.Next = &ListNode{Val: currentNode.Val}
-					resultCurrent = resultCurrent.Next
-				}
-				existMap[currentNode.Val] = nil
-			}
+	cur := head
+	for cur != nil {
+		if cur.Next != nil && cur.Val == cur.Next.Val {
+			cur.Next = cur.Next.Next
+			continue
 		}
-		currentNode = currentNode.Next
+		cur = cur.Next
 	}
 
-	return resultHead
+	return head
+
+	// ------------------------以下是第一次寫的大便------------------------
+	// if head == nil {
+	// 	return nil
+	// }
+	// var resultHead, resultCurrent *ListNode
+	// existMap := map[int]interface{}{}
+
+	// currentNode := head
+	// for {
+	// 	if currentNode == nil {
+	// 		break
+	// 	} else {
+	// 		if _, exist := existMap[currentNode.Val]; !exist {
+	// 			if resultHead == nil {
+	// 				resultHead = &ListNode{Val: currentNode.Val}
+	// 				resultCurrent = resultHead
+	// 			} else {
+	// 				resultCurrent.Next = &ListNode{Val: currentNode.Val}
+	// 				resultCurrent = resultCurrent.Next
+	// 			}
+	// 			existMap[currentNode.Val] = nil
+	// 		}
+	// 	}
+	// 	currentNode = currentNode.Next
+	// }
+
+	// return resultHead
 }
